a9a80853d013ce62b8543c47567ca9f22b62997d
Art-_-master <ggn.master@gmail.com>
2017-11-21 21:58:00 +0300

    Add class for drive the knight. Refactor code

--- java/com/app/art_master/chessknight/DarwRectView.java
+++ java/com/app/art_master/chessknight/DarwRectView.java
@@ -4,61 +4,107 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Picture;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
 /**
- *
- * Created by Art-_-master on 18.11.2017.
+ * Прорисовывает шахматную доску, а так же выполняет отрисовку других элементов
+ * Created by Art-_-master.
  */
 
 public class DarwRectView extends View{
-    Paint p;
-    Rect rect;
 
-    RectF mRectF;
+    /** Объект Paint. Устанавливает параметры отрисовки */
+    private Paint mPaint;
 
-    int numCollumn;
-    int numCell;
+    /** Отрисовывает поля для хода на шахматной доске */
+    private Rect mRect;
 
-    boolean init=true;
+    /** Отрисовывает метки на полях для хода */
+    //private Ova mCircle;
 
-    public static int [][][] rectsCoordinate;
+    /** Количество столбцов в доске */
+    private int mNumColumn;
 
-    public DarwRectView(Context context, AttributeSet atr, int column, int cell) {
+    /** Количество линий в доске */
+    private int mNumCell;
+
+    /** Координаты полей для хода */
+    public static int [][][] mRectsCoordinates;
+
+    /** Объект Picture. Для сохранения шахматной доски в доп. слое */
+    private Picture mBoardLayer;
+
+    /** Высота текущего View */
+    private  int mHeight;
+
+    /** Ширина текущего View */
+    private  int mWidth;
+
+    /** Разрешает перерисокву Canvas */
+    private boolean mReDraw=false;
+
+    public DarwRectView(Context context, AttributeSet atr, int column, int cell, int height, int width) {
         super(context);
-        setLayerType(LAYER_TYPE_SOFTWARE, null);
-        numCell=cell;
-        numCollumn=column;
-        p = new Paint();
-        rect = new Rect();
 
-        mRectF = new RectF(rect);
+        mNumCell =cell;
+        mNumColumn =column;
+        mHeight=height;
+        mWidth=width;
 
-        rectsCoordinate = new int[cell][column][2];
-        rectsCoordinate[0][0][0]=0;
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        mRect = new Rect();
+
+        mRectsCoordinates = new int[cell][column][2];
+
+        mRectsCoordinates[0][0][0]=0;
+
+        drawBoard(width, height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        // заливка канвы цветом
 
-        if(init){
-            int rectWidth=getHeight()/numCollumn;
-            int rectHeight= getHeight()/numCell;
+        canvas.drawPicture(mBoardLayer);
+
+        if(mReDraw){
+            mRect. set(0, mHeight/ mNumColumn, mHeight/ mNumCell, 0);
+            mPaint.setColor(Color.GREEN);
+            //canvas.drawRect(mRect, mPaint);
+            RectF rectF= new RectF(mRect);
+            //rectF.set(mRect);
+            canvas.drawOval(rectF, mPaint);
+        }
+    }
+
+    /**
+     * Отрисовывает шахматную доску в зависимости от введенных данных пользователем
+     * и сохраняет ее в отдельном слое
+     *
+     * @param width  - ширина текущего View
+     * @param height - высота текущего View
+     */
+    private void drawBoard(int width, int height){
+            int rectWidth=height/ mNumColumn;
+            int rectHeight= height/ mNumCell;
+
+            mBoardLayer = new Picture();
+            Canvas canvas = mBoardLayer.beginRecording(width, height);
 
             int alternation;
 
-            for(int i=0; i<numCell; i++) {
-                for (int i1 = 0; i1 < numCollumn; i1++) {
-                    rectsCoordinate[i][i1][0]= rectsCoordinate[i][i1][0]+rectWidth*i1;
-                    rectsCoordinate[i][i1][1]= rectsCoordinate[i][i1][1]+rectHeight*i;
-                    rect.set(rectsCoordinate[i][i1][0],
-                            rectsCoordinate[i][i1][1] + rectHeight,
-                            rectsCoordinate[i][i1][0] + rectWidth,
-                            rectsCoordinate[i][i1][1]);
+            for(int i = 0; i< mNumCell; i++) {
+                for (int i1 = 0; i1 < mNumColumn; i1++) {
+                    mRectsCoordinates[i][i1][0]= mRectsCoordinates[i][i1][0]+rectWidth*i1;
+                    mRectsCoordinates[i][i1][1]= mRectsCoordinates[i][i1][1]+rectHeight*i;
+                    mRect.set(mRectsCoordinates[i][i1][0],
+                            mRectsCoordinates[i][i1][1] + rectHeight,
+                            mRectsCoordinates[i][i1][0] + rectWidth,
+                            mRectsCoordinates[i][i1][1]);
 
                     if (i%2==0) {
                         alternation=1;
@@ -69,30 +115,31 @@ public class DarwRectView extends View{
                     {
                         case 0:
                             if (i1%2!=0) {
-                                p.setColor(Color.WHITE);
+                                mPaint.setColor(Color.WHITE);
                             }else{
-                                p.setColor(Color.BLACK);
+                                mPaint.setColor(Color.BLACK);
                             }
                             break;
                         case 1:
                             if (i1%2!=0) {
-                                p.setColor(Color.BLACK);
+                                mPaint.setColor(Color.BLACK);
                             }
                             else{
-                                p.setColor(Color.WHITE);
+                                mPaint.setColor(Color.WHITE);
                             }
                             break;
                     }
 
-                    // настройка кисти
-                    // рисуем прямоугольник из объекта rect
-                    canvas.drawRect(rect, p);
-
+                    // рисуем прямоугольник из объекта mRect
+                    canvas.drawRect(mRect, mPaint);
                 }
             }
-            //canvas.saveLayer(mRectF, p, Canvas.ALL_SAVE_FLAG);
+        mBoardLayer.endRecording();
         }
 
+    public void click(){
+        mReDraw=true;
+        invalidate();
     }
 
 }
