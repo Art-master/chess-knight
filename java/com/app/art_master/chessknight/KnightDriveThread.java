89eb6a0d3176bd163756c12a3c05db2704bae18c
Art-_-master <ggn.master@gmail.com>
2017-12-14 21:48:39 +0300

    Change algorithm move knight.

--- java/com/app/art_master/chessknight/KnightDriveThread.java
+++ java/com/app/art_master/chessknight/KnightDriveThread.java
@@ -41,13 +41,8 @@ public class KnightDriveThread implements Runnable {
      *Счетчики и инициаторы
      */
     private boolean mKnightMoveControl =false;
+
     private boolean mControl =true;
-
-    /** объект шахматной доски в Canvas и анимации движения коня по доске */
-   // private DarwRectView mChessboard;
-
-    /** матрица рпзрешенных ходов коня по доске */
-    private boolean[][][] mMatrixSteps;
 
     /** Handler для отслеживания действий паралельного потока*/
     private HandlerPermissionStart mHandler;
@@ -61,7 +56,6 @@ public class KnightDriveThread implements Runnable {
 
         matrixChessBoard = new int[mColumn][mCell][1];
         mLastKnightSteps = new int[mColumn * mCell +1];
-        mMatrixSteps = new boolean [mColumn][mCell][8];
 
         //В начале работы алгоритма получаем индексы матрицы, с текущими координатами коня
         mKnightMoveControl =true;
@@ -82,47 +76,31 @@ public class KnightDriveThread implements Runnable {
     public void present() {
 
         if(mKnightMoveControl){
-            //количество обработанных алгаритмом ходов коня
-            int controlBackStep = 0;
-            //
             int forward;
             matrixChessBoard[mInitArray1][mInitArray2][0]=1;
             while(mControl){
 
-                /*
-                  *
-                  *
-                  *
-                  * */
                 int[] steps1 ={2, 2,  -2,  -2, 1, -1, 1, -1};
                 int[] steps2 ={-1, 1,  -1, 1, -2, -2, 2,  2};
 
                 //находим доступные ходы
-                forward = moveKnightInCell(controlBackStep, steps1, steps2);
-
+                forward = moveKnightInCell(steps1, steps2);
+                if(forward==0xFF){
+                    matrixChessBoard[0][0][0]=0;
+                    break;
+                }
+                // записываем координаты следующей точки в зависимости от того, куда шагнул конь
+                mInitArray1 += steps1[forward];
+                mInitArray2 += steps2[forward];
+                actionToStepKnight(forward);
                 // перед каждым ходом обнуляем щетчик ходов
-                controlBackStep = 0;
+                //controlBackStep = 0;
 
-
-                //всего 8 возможных ходов коня по доске из заданной точки
-                // i= шаг
-
-                interrupt: for (int i = forward; i <8; i++) {
-                    //если текущий шаг не запрещен
-                    if (mMatrixSteps[mInitArray1][mInitArray2][i]) {
-                        //i=forward;
-                        // записываем координаты следующей точки в зависимости от того, куда шагнул конь
-                            mInitArray1 += steps1[i];
-                            mInitArray2 += steps2[i];
-                            actionToStepKnight(i);
-                            break interrupt;
-                    } else {
-
-                        // Если по окончания цикла, ходов не обнаружено,
+             /*           // Если по окончания цикла, ходов не обнаружено,
                         // подготавливаемся к ходу назад
                         controlBackStep++;
                         if (controlBackStep == 7) {
-                            matrixChessBoard[0][0][0]=0;
+                            //matrixChessBoard[0][0][0]=0;
                        /*     // счетчик всех ходов уменьшаем на единицу
                             mPathKnight--;
 
@@ -138,15 +116,15 @@ public class KnightDriveThread implements Runnable {
                                         }
                                     }
                                 }
-                            } */
+                            }
                             // Если ход не удался, ищем в массиве значение с
                             // предыдущим path и переходим на нее
                             // перезаписываем массив ходов коня
-                            break interrupt;
+                           // break interrupt;
                         }
                     }
 
-                }
+                }*/
             }
             Message msg=new Message();
             msg.arg2=3;
@@ -184,106 +162,52 @@ public class KnightDriveThread implements Runnable {
         }
     }
 
-    private int moveKnightInCell(int controlBackStep, int[] steps1, int[] steps2) {
-        int a = 0;
+    private int moveKnightInCell(int[] steps1, int[] steps2) {
+        int a = 0xff;
         int t = 0;
         int valuePath = 8;
 
-        for (int y = 0; y <= mColumn -1; y++) {
-            for (int y2 = 0; y2 <= mCell -1; y2++) {
-                for (int y3 = 0; y3 <= 7; y3++) {
-                    mMatrixSteps[y][y2][y3] = true;
-                }
-            }
-        }
-        //запускаем цикл, в котором проверяем возможные шаги коня
         for (int i = 0; i <= 7; i++) {
-            if (mInitArray1 + steps1[i] > mColumn - 1 || mInitArray1 + steps1[i]<0 ) {
-                mMatrixSteps[mInitArray1][mInitArray2][i] = false;
-            }else if (mInitArray2 + steps2[i]  < 0  ||  mInitArray2 + steps2[i]> mCell - 1) {
-                mMatrixSteps[mInitArray1][mInitArray2][i] = false;
-            }else if (matrixChessBoard[mInitArray1 + steps1[i]][mInitArray2 +steps2[i]][0] > 0) {
-                mMatrixSteps[mInitArray1][mInitArray2][i] = false;
-            }else if (controlBackStep==8) {
-                int q = mLastKnightSteps[mPathKnight];
-                mMatrixSteps[mInitArray1][mInitArray2][q] = false;
-            }
 
-            //Отправляеем коня в будущее
-            else if (mMatrixSteps[mInitArray1][mInitArray2][i]) {
-                mLocalInit1 = mInitArray1 + steps1[i];
-                mLocalInit2 = mInitArray2 + steps2[i];
+            int stepHorizontal = mInitArray1 + steps1[i];
+            int stepVertical = mInitArray2 + steps2[i];
+            //если шаг коня выходит за границы или конь уже бывал в этой клетке
+            if (checkStep(stepHorizontal, stepVertical)) {
+                //проверяем вес каждого хода коня
                 for (int i2 = 0; i2 <= 7; i2++) {
-                    if (mLocalInit1 + steps1[i2] > mColumn - 1 || mLocalInit1 + steps1[i2]<0  ) {
-                        mMatrixSteps[mLocalInit1][mLocalInit2][i2] = false;
-                    } else if (mLocalInit2 + steps2[i2]> mCell - 1  || mLocalInit2 + steps2[i2]  < 0  ) {
-                        mMatrixSteps[mLocalInit1][mLocalInit2][i2] = false;
-                    } else if (matrixChessBoard[mLocalInit1 + steps1[i2]][mLocalInit2 +steps2[i2]][0] > 0) {
-                        mMatrixSteps[mLocalInit1][mLocalInit2][i2] = false;
-                    }else if (mMatrixSteps[mLocalInit1][mLocalInit2][i2]) {
+                    mLocalInit1 = stepHorizontal + steps1[i2];
+                    mLocalInit2 = stepVertical + steps2[i2];
+                    //если шаг коня выходит за границы
+                    if (checkStep(mLocalInit1, mLocalInit2)) {
                         t++;
                     }
                 }
             }
-            if (t < valuePath & t>0) {
-                valuePath = t;
-                a=i;
-            } else {
-                mMatrixSteps[mLocalInit1][mLocalInit2][i] = false;
+                //выбираем клетку с наименьшим весом, но вес должен быть больше ноля
+                if (t < valuePath & t>0) {
+                    valuePath = t;
+                    a = i;
+                }
+                t = 0;
             }
-            t=0;
-        }
+            //если ходов никаких не обнаружено, то функция вернет 0xFF,
+            //в ином случае вернет номер хода
         return a;
-
     }
-    /*
-    private int moveKnightInCell(int controlBackStep, int[] steps1, int[] steps2) {
-        int a = 0xFF; 
-        int t = 0;
-        int valuePath = 8;
 
-      	matrixSteps = new boolean [this.column][this.cell][8];
-		matrixSteps[0][0][0]=false;
-		
-		for (int i = 0; i <= 7; i++) {
-			
-			int stepHorizontal=mInitArray1 + steps1[i];
-			int stepVertical=mInitArray2 + steps2[i];
-			//если шаг коня выходит за границы или конь уже бывал в этой клетке
-			if (!checkStep(stepHorizontal, stepVertical) || mMatrixSteps[mInitArray1][mInitArray2][i]) {
-                mMatrixSteps[mInitArray1][mInitArray2][i] = false;
-			}else if (controlBackStep==8) {
-                int q = mLastKnightSteps[mPathKnight];
-                mMatrixSteps[mInitArray1][mInitArray2][q] = false;
-            }
-			else{
-				//проверяем вес каждого хода коня
-			    mLocalInit1 = mInitArray1 + steps1[i];
-                mLocalInit2 = mInitArray2 + steps2[i];
-                for (int i2 = 0; i2 <= 7; i2++) {
-					//если шаг коня выходит за границы
-					if (!checkStep(mLocalInit1, mLocalInit2)) {
-						mMatrixSteps[mLocalInit1][mLocalInit2][i2] = false;
-					//если конь уже бывал в этой клетке
-					else if(mMatrixSteps[mInitArray1][mInitArray2][i2]){
-						t++;
-					}else{
-						mMatrixSteps[mInitArray1][mInitArray2][i2]=true;
-					}
-				}	
-			}
-			//выбираем клетку с наименьшим весом, но вес должен быть больше ноля
-            if (t < valuePath & t>0) {
-                valuePath = t;
-                a=i;
-            }
-            t=0;			
-		}
-		//если ходов никаких не обнаружено, то функция вернет 0xFF,
-		//в ином случае вернет номер хода
-		return a;
-		}
-    */
+    private boolean checkStep(int stepHorizontal, int stepVertical){
+        boolean q;
+        if (stepHorizontal > mColumn - 1 || stepHorizontal < 0) {
+            q= false;
+        }else if(stepVertical < 0 || stepVertical> mCell - 1){
+            q= false;
+        }else if(matrixChessBoard[stepHorizontal][stepVertical][0]>0){
+            q= false;
+        }else{
+            q= true;
+        }
+        return q;
+    }
 
     /**
      * Устанавливает обработчик
